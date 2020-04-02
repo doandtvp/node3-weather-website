3b4238fa3045861a4ac98fd6158532f6e32af980
doan <doandtvp@gmail.com>
2020-04-02 19:47:37 +0700

    set up app for Heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Setup handlebars enginee and views location 
 app.set('view engine', 'hbs')
@@ -81,6 +82,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
