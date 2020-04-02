e0597bd1b26a71fa38342c5021abfc9f9d9fe3d2
doan <doandtvp@gmail.com>
2020-04-02 20:16:30 +0700

    spelling correction

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/help', (req, res) => {
-    res.render('Help', {
+    res.render('help', {
         title: 'Help',
         helpText: 'This is help page content.',
     })
